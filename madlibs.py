1ad26c58308384c3c355b8b597a8b331cad0c90f
Tharushi-Nathasha <taashiiyaa@gmail.com>
2025-09-12 14:37:47 +0530

    Addded Mad Lib Game project Phase one

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+
+adjective = input("Adjective: ")
+verb = input("Verb: ")
+noun = input("Noun:")
+famous_person = input("Famous Person: ")
+place = input("Place: ")
+
+madlib = f"I love making {adjective} content! It makes me {verb} when ever I create them. I love being financially stable and a {adjective} content creator. I want to be as famous as {famous_person} and buy my favourite items and to decorate my {place}."
+print(madlib)
+
