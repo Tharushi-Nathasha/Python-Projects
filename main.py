e610df524fa30169fa077cb30f842d979e81b99f
Tharushi-Nathasha <taashiiyaa@gmail.com>
2025-09-23 08:41:52 +0530

    Implement maximum attempts for guessing and enhance feedback messages

--- main.py
+++ main.py
@@ -3,12 +3,29 @@ import random
 def guess(x):
     random_number = random.randint(1,x)
     guess = 0
-    while guess != random_number:
+    attempts = 0
+
+    #player has 5 attempts maximum to guess the number
+    while guess != random_number and attempts < 5:
+        attempts += 1
         guess = int(input(f'Guess an number between 1 and {x} : '))
         if guess < random_number:
             print('Sorry, guess again. Too low.')
         elif guess > random_number:
             print('Sorry, guess again. Too high.')
-    print(f'Yay, congrats. You have guessed the number {random_number} correctly!!')
+
+if guess == random_number:
+        print(f'Yay, congrats. You have guessed the number {random_number} correctly in {attempts} attempts!')
+else:
+        print(f'Sorry, you have used all your attempts. The number was {random_number}. Better luck next time!')
+
+#Ask to play again
+play_again = input('Do you want to play again? (y/n): ')
+if play_again.lower() == 'y':
+    guess(x)
 
 guess(10)
+
+
+    
+    
